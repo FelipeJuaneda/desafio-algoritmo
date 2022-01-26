79822e8a869396602314c43973eca7a687eba409
felipe.juaneda <felipejua9@gmail.com>
2022-01-26 16:51:53 -0300

    Algoritmo correjido- con funciones condicional y ciclos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+/* Verificacion de edades */
+function verificarEdad(){
+    let edad = document.getElementById("edad").value;
+
+    if ((edad!="") && (edad>18)) {
+        alert("Bienvenido!" + " tu edad es de " + edad+ " años")
+    }else{
+        alert('DATOS INCORRECTOS - No eres mayor de 18')
+    }
+}
+
+/* Algoritmo promedio de edades */
 let cantidadUsuarios = parseInt(prompt("Indique cuantos usuarios se registraran"));
 let sumaDeUsuarios = 0;
 let n;
